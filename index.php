e3b20ec8c6eb9a75ac6f23967736501bd2d03f9f
imaarov <imanatarof@gmail.com>
2023-08-21 02:52:27 +0330

    remove uri

--- index.php
+++ index.php
@@ -2,5 +2,4 @@
 require_once 'vendor/autoload.php';
 require_once 'route/web.php';
 
-$uri = new \Imaarov\Utils\UriSeparation($_SERVER['REQUEST_URI']);
 \Imaarov\Utils\Router::dispatch($_SERVER['REQUEST_URI']);
